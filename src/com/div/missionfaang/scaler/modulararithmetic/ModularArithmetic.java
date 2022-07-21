d6b3bed6ac548912701d3e323d234d57ce0a647f
Divakar Saripalli <neelakantam-divakar.saripalli@broadcom.com>
2022-07-21 16:12:03 +0530

    Modular Arithmetic

--- src/com/div/missionfaang/scaler/modulararithmetic/ModularArithmetic.java
+++ src/com/div/missionfaang/scaler/modulararithmetic/ModularArithmetic.java
@@ -1,24 +1,63 @@
 package com.div.missionfaang.scaler.modulararithmetic;
 
 public class ModularArithmetic {
-    private static int divisibilityBy8(String A) {
-        if (A.isEmpty() || A.isBlank()) {
-            return 0;
-        }
-        if (A.length() < 2 && (Integer.parseInt(A) != 8 && Integer.parseInt(A) != 0)) {
-            return 0;
-        }
 
-        if (A.length() > 1) {
+    private static int divisibilityBy8(String A) {
+        int result = 1;
+        if (A.isEmpty() || A.isBlank()) {
+            result = 0;
+        } else if (A.length() < 2 && (Integer.parseInt(A) != 8 && Integer.parseInt(A) != 0)) {
+            result = 0;
+        } else if (A.length() > 1) {
             if (Integer.parseInt("" + A.charAt(A.length() - 1)) % 2 != 0) {
-                return 0;
-            }
-            int lastTwoDigits = Integer.parseInt(A.substring(A.length() - 2));
-            if (lastTwoDigits % 4 != 0 || (lastTwoDigits / 4) % 2 != 0) {
-                return 0;
+                result = 0;
+            } else {
+                int digitsToConsider;
+                if (A.length() < 3) {
+                    digitsToConsider = Integer.parseInt(A);
+                } else {
+                    digitsToConsider = Integer.parseInt(A.substring(A.length() - 3));
+                }
+                if (digitsToConsider % 4 != 0 || (digitsToConsider / 4) % 2 != 0) {
+                    result = 0;
+                }
             }
         }
-        return 1;
+        return result;
+    }
+
+    private static int concatenateThreeNumbers(int A, int B, int C) {
+        int result;
+        if (A <= B) {
+            result = ModularArithmetic.ifALessThanBAndC(A, B, C);
+        } else {
+            result = ModularArithmetic.ifALessThanBAndC(B, A, C);
+        }
+        return result;
+    }
+
+    private static int ifALessThanBAndC(int A, int B, int C) {
+        int result;
+        if (A <= C) {
+            result = A;
+            result *= 100;
+            if (B <= C) {
+                result += B;
+                result *= 100;
+                result += C;
+            } else {
+                result += C;
+                result *= 100;
+                result += B;
+            }
+        } else {
+            result = C;
+            result *= 100;
+            result += A;
+            result *= 100;
+            result += B;
+        }
+        return result;
     }
 
     private static int abAndModulo(int A, int B) {
@@ -33,6 +72,7 @@ public class ModularArithmetic {
     }
 
     public static void main(String[] args) {
-        System.out.println(ModularArithmetic.divisibilityBy8("0"));
+//        System.out.println(ModularArithmetic.divisibilityBy8("0"));
+        System.out.println(ModularArithmetic.concatenateThreeNumbers(74, 86, 54));
     }
 }
