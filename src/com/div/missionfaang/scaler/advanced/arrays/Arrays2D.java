170ff561af6629d2368343ea237248673c39bb2b
Divakar Saripalli <neelakantam-divakar.saripalli@broadcom.com>
2022-08-27 23:56:02 +0530

    Advanced matrices and array problems.

--- src/com/div/missionfaang/scaler/advanced/arrays/Arrays2D.java
+++ src/com/div/missionfaang/scaler/advanced/arrays/Arrays2D.java
@@ -55,25 +55,7 @@ public class Arrays2D {
     }
 
     private static int maxSumSubMatrix(ArrayList<ArrayList<Integer>> A) {
-        ArrayList<ArrayList<Integer>> prefixSumMatrix = new ArrayList<>(A.size());
-        for (ArrayList<Integer> integers : A) {
-            ArrayList<Integer> row = new ArrayList<>();
-            row.add(integers.get(0));
-            prefixSumMatrix.add(row);
-        }
-
-        for (int i = 0; i < A.size(); i++) {
-            for (int j = 1; j < A.get(i).size(); j++) {
-                prefixSumMatrix.get(i).add(prefixSumMatrix.get(i).get(j - 1) + A.get(i).get(j));
-            }
-        }
-
-        for (int i = 1; i < A.size(); i++) {
-            for (int j = 0; j < A.get(i).size(); j++) {
-                prefixSumMatrix.get(i).set(j, prefixSumMatrix.get(i - 1).get(j) + prefixSumMatrix.get(i).get(j));
-            }
-        }
-
+        ArrayList<ArrayList<Integer>> prefixSumMatrix = Arrays2D.generatePrefixSumMatrix(A);
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < A.size(); i++) {
             for (int j = 0; j < A.get(i).size(); j++) {
@@ -115,28 +97,31 @@ public class Arrays2D {
         return -1;
     }
 
-    public static void main(String[] args) {
-//        int[][] arr1 = new int[][]{
-//                {8, 9, 9, 1, 7},
-//                {5, 5, 10, 1, 0},
-//                {7, 7, 5, 8, 6},
-//                {7, 3, 7, 9, 2},
-//                {7, 7, 8, 10, 6}
-//        };
-
-        int[][] arr1 = new int[][]{
-                {-6, -21, 27, 19, 19},
-                {0, 0, 5, -21, 19},
-                {18, -27, -2, -7, 13},
-                {-21, -17, -25, -1, 3},
-                {0, -9, -6, -16, -5},
-                {29, 9, -25, -7, -25}
-        };
-        ArrayList<ArrayList<Integer>> matrix = ArrayUtility.convert2DArrayTo2DList(arr1);
-        System.out.println(Arrays2D.maxSumSubMatrix(matrix));
+    private static ArrayList<Integer> subMatrixSumQueries(ArrayList<ArrayList<Integer>> A, ArrayList<Integer> B, ArrayList<Integer> C, ArrayList<Integer> D, ArrayList<Integer> E) {
+        ArrayList<ArrayList<Integer>> prefixSumMatrix = Arrays2D.generatePrefixSumMatrix(A);
+        System.out.println(prefixSumMatrix);
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < B.size(); i++) {
+            int sum = prefixSumMatrix.get(D.get(i) - 1).get(E.get(i) - 1);
+            sum %= 1000000007;
+            if (B.get(i) > 1) {
+                sum -= prefixSumMatrix.get(B.get(i) - 2).get(E.get(i) - 1);
+                sum %= 1000000007;
+            }
+            if (C.get(i) > 1) {
+                sum -= prefixSumMatrix.get(D.get(i) - 1).get(C.get(i) - 2);
+                sum %= 1000000007;
+            }
+            if (B.get(i) > 1 && C.get(i) > 1) {
+                sum += prefixSumMatrix.get(B.get(i) - 2).get(C.get(i) - 2);
+                sum %= 1000000007;
+            }
+            result.add((sum % 1000000007));
+        }
+        return result;
     }
 
-    public ArrayList<Integer> subMatrixSumQueries(ArrayList<ArrayList<Integer>> A, ArrayList<Integer> B, ArrayList<Integer> C, ArrayList<Integer> D, ArrayList<Integer> E) {
+    private static ArrayList<ArrayList<Integer>> generatePrefixSumMatrix(ArrayList<ArrayList<Integer>> A) {
         ArrayList<ArrayList<Integer>> prefixSumMatrix = new ArrayList<>(A.size());
         for (ArrayList<Integer> integers : A) {
             ArrayList<Integer> row = new ArrayList<>();
@@ -155,21 +140,38 @@ public class Arrays2D {
                 prefixSumMatrix.get(i).set(j, prefixSumMatrix.get(i - 1).get(j) + prefixSumMatrix.get(i).get(j));
             }
         }
+        return prefixSumMatrix;
+    }
 
-        ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < B.size(); i++) {
-            int sum = prefixSumMatrix.get(D.get(i) - 1).get(E.get(i) - 1);
-            if (B.get(i) > 1) {
-                sum -= prefixSumMatrix.get(B.get(i) - 2).get(E.get(i) - 1);
-            }
-            if (C.get(i) > 1) {
-                sum -= prefixSumMatrix.get(D.get(i) - 1).get(C.get(i) - 2);
-            }
-            if (B.get(i) > 1 && C.get(i) > 1) {
-                sum += prefixSumMatrix.get(B.get(i) - 2).get(C.get(i) - 2);
-            }
-            result.add((sum % 1000000007));
-        }
-        return result;
+    public static void main(String[] args) {
+//        int[][] arr1 = new int[][]{
+//                {8, 9, 9, 1, 7},
+//                {5, 5, 10, 1, 0},
+//                {7, 7, 5, 8, 6},
+//                {7, 3, 7, 9, 2},
+//                {7, 7, 8, 10, 6}
+//        };
+
+        int[][] arr1 = new int[][]{
+//                {-6, -21, 27, 19, 19},
+//                {0, 0, 5, -21, 19},
+//                {18, -27, -2, -7, 13},
+//                {-21, -17, -25, -1, 3},
+//                {0, -9, -6, -16, -5},
+//                {29, 9, -25, -7, -25}
+                {1, 2, 3},
+                {1, 2, 3},
+                {1, 2, 3}
+        };
+        ArrayList<Integer> B = new ArrayList<>();
+        B.add(1);
+        ArrayList<Integer> C = new ArrayList<>();
+        C.add(1);
+        ArrayList<Integer> D = new ArrayList<>();
+        D.add(2);
+        ArrayList<Integer> E = new ArrayList<>();
+        E.add(2);
+        ArrayList<ArrayList<Integer>> matrix = ArrayUtility.convert2DArrayTo2DList(arr1);
+        System.out.println(Arrays2D.subMatrixSumQueries(matrix, B, C, D, E));
     }
 }
